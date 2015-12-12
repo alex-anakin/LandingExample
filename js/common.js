895ecfacc861d9442134ab3e46bb4fd98fe1c27d
BelogurovaOlga <obelogurova2@gmail.com>
2015-12-12 22:14:26 +0200

    added scroll function

--- js/common.js
+++ js/common.js
@@ -7,4 +7,21 @@ $(document).ready(function() {
         }
     } catch(err) {/* Do nothing */}
 
+    $(function () {
+        var currentHash = "#home";
+        $(document).scroll(function () {
+            $('.page-scroll').each(function () {
+                var top = window.pageYOffset;
+                var distance = top - $(this).offset().top;
+                var hash = $(this).attr('href');
+
+                if (distance < 30 && distance > -30 && currentHash != hash) {
+                    alert(hash);
+                    currentHash = hash;
+                    /*history.pushState(null, null, currentHash);*/
+                }
+            });
+        });
+    });
+
 });
